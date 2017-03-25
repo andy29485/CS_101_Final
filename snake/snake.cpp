34b2a2525368d598e710d09547273c341191215e
Andriy Zasypkin <AndriyZasypkin@gmail.com>
2017-03-25 17:51:45 -0400

    added placeholder files for the snake game

---
+++ snake/snake.cpp
@@ -0,0 +1,9 @@
+//CS101 Final Project
+//Group:   12
+//file:    snake.cpp
+//date:    2017-03-25
+//authors: Andriy Zasypkim
+
+#import "snake.hpp"
+
+//TODO
---
+++ snake/snake.hpp
@@ -0,0 +1,12 @@
+//CS101 Final Project
+//Group:   12
+//file:    snake.hpp
+//date:    2017-03-25
+//authors: Andriy Zasypkim
+
+#ifndef SNAKE_HPP
+#define SNAKE_HPP
+
+//TODO
+
+#endif
